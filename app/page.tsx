a4f6142e0e0211d99e6592c2cb231707c36b8f2c
Dominier <118577440+TimCooks@users.noreply.github.com>
2023-12-04 20:49:48 -0800

    Install packages + constants.ts and route.ts

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import Image from 'next/image';
 import { velocity } from '@/utils/velocity';
+import InputTable from '@/components/input-table';
 
 export default function Home() {
   let velocityTest = velocity();
@@ -9,11 +12,15 @@ export default function Home() {
       <div className="font-bold text-xl text-center pt-5 border border-black">
         Physics Project Very Rough Draft
       </div>
+
       <div className="grid lg:grid-cols-2 pl-14 pr-14 mt-[4%]">
         <div className="border border-blue-600 text-center"><a href="/linechart">Kinematics</a></div>
         <div className="border border-red-600 text-center">Projectile Motion</div>
         {velocityTest}
       </div>
+
+      <InputTable />
+
     </div>
   )
 }
---
+++ components/input-table.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { useState } from "react";
+
+const InputTable = async () => {
+    
+
+    return (
+        <>
+            TABLE
+        </>
+    )
+}
+
+export default InputTable;
