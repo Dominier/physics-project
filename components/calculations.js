6c6dfdc9e46dcd13e9ac495c65fbb5b6292963b4
Dominier <118577440+TimCooks@users.noreply.github.com>
2023-12-06 01:22:06 -0800

    moved ai generation to calculation.js

--- components/calculations.js
+++ components/calculations.js
@@ -1,5 +1,9 @@
-/* CALCULATIONS */
+/* CALCULATIONS AND AI */
+import { useState } from "react";
+
 import { parseSubmittedValue } from "../utils/parseSubmittedValue";
+
+const API_KEY = "API KEY"
 
 const Calculations = ({ submittedValue }) => {
   const parsedData = parseSubmittedValue(submittedValue);
@@ -16,10 +20,67 @@ const Calculations = ({ submittedValue }) => {
   // Calculate average velocity
   const velocity = totalDisplacement / totalTime;
 
+
+  const [description, setDescription] = useState("");
+  const [report, setReport] = useState(""); // some words
+
+  async function callOpenAIAPI() {
+    console.log("Calling the OpenAI API");
+
+    const APIBody = {
+        "model": "gpt-4-1106-preview",
+        "messages": [
+            {"role": "system", "content": "You are a helpful assistant that generate lab reports based on the information you are given."},
+            {"role": "user", "content": `Generate a lab report with the following short description: ${description}. Create a title, objective, and conclusion.`},
+        ],
+        "temperature": 0.8,
+        "top_p": 1
+    };
+
+    try {
+        const response = await fetch("https://api.openai.com/v1/chat/completions", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${API_KEY}`
+            },
+            body: JSON.stringify(APIBody)
+        });
+
+        const data = await response.json();
+        console.log(data);
+
+        // Update state with the generated report
+        setReport(data.choices[0].message.content);
+    } catch (error) {
+        console.error("Error calling OpenAI API:", error);
+    }
+}
+
+
   return (
     <div className="pt-5">
       <h2 className="font-bold">Calculations</h2>
       <p>Average Velocity: {velocity} m/s</p>
+
+      <h3 className="font-bold">TESTING</h3>
+            <div>
+                <textarea
+                    onChange={(e) => setDescription(e.target.value)}    
+                    placeholder="Paste your short summary here"
+                    cols={30} 
+                    rows={10} 
+                    className="border border-black" 
+                />
+            </div>
+            <div>
+                <button onClick={callOpenAIAPI} className="border border-black p-2" >Generate from AI</button>
+            </div>
+            {report !== "" ? 
+                <h3>lab report: {report}</h3>
+                : null    
+            }
+
     </div>
   );
 };
